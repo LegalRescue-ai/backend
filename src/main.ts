625f36f265d8a9d684f32e9b8f37cfd317120dfb
Tonnel <154825284+T0nnel@users.noreply.github.com>
2025-02-13 01:21:48 +0300

    making changes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-require-imports */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -8,28 +6,29 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
-const cors = require('cors');
-app.use(cors({ 
-  origin:
-  [
-   'http://localhost:3000', 
-   'https://tonnel-test-client.d34j9mhleth3x6.amplifyapp.com']
-}));
+  const app = await NestFactory.create(AppModule);
+
+  // Enable CORS correctly
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', 
+      'https://tonnel-test-client.d34j9mhleth3x6.amplifyapp.com'
+    ],
+    credentials: true, // If using cookies or authorization headers
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  // Global validation pipe
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-    transform:true,
-    forbidNonWhitelisted:true,
-    transformOptions: {
-      enableImplicitConversion: true,
-    },
-    validationError: {
-      target: false,
-      value: false,
-    },
-  }))
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+    transformOptions: { enableImplicitConversion: true },
+    validationError: { target: false, value: false },
+  }));
+
   await app.listen(process.env.PORT ?? 3001);
   console.log(`Server is running on port ${process.env.PORT ?? 3001}`);
-
 }
 bootstrap();
