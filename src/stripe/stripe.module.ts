664c1a9656625121c2664b908bc36c297560db23
samuelkamotho92 <samuelkamotho92@gmail.com>
2025-03-06 01:14:32 +0300

    fixing stripe issues

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -5,8 +5,8 @@ import { PaymentController } from 'src/payment/payment.controller';
 import { SupabaseService } from 'src/supabase/supabase.service';
 
 @Module({
-    imports: [],
-    controllers: [PaymentController],
-    providers: [StripeService, SupabaseService],
+  imports: [],
+  controllers: [PaymentController],
+  providers: [StripeService, SupabaseService],
 })
 export class StripeModule {}
